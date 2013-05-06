a13829ff12bd9e5ba98ad9d9d47136225d35f9fc
Jens Andersson <kobingo@gmail.com>
2013-05-06 12:59:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeReport::Application.config.secret_token = '52fc7416f3a590324d7a0e204d55b9ad3883ae81b51013bd1144d43b7d5bd92ec54ac916a0fe92da237c35e2e0a62046fea6f94b0e8ae2db6794a5e24066c01b'
